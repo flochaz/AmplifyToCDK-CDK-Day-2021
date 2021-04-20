fe938e694ce92f84016b260efdc4cab9385c4604
Florian CHAZAL <chazalf@amazon.com>
2021-04-20 08:34:36 +0200

    chore: clone initial amplify App

---
+++ amplifyApp/src/GraphQL/mutations.js
@@ -0,0 +1,63 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createPicture = /* GraphQL */ `
+  mutation CreatePicture(
+    $input: CreatePictureInput!
+    $condition: ModelPictureConditionInput
+  ) {
+    createPicture(input: $input, condition: $condition) {
+      id
+      name
+      owner
+      visibility
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updatePicture = /* GraphQL */ `
+  mutation UpdatePicture(
+    $input: UpdatePictureInput!
+    $condition: ModelPictureConditionInput
+  ) {
+    updatePicture(input: $input, condition: $condition) {
+      id
+      name
+      owner
+      visibility
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deletePicture = /* GraphQL */ `
+  mutation DeletePicture(
+    $input: DeletePictureInput!
+    $condition: ModelPictureConditionInput
+  ) {
+    deletePicture(input: $input, condition: $condition) {
+      id
+      name
+      owner
+      visibility
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
---
+++ amplifyApp/src/GraphQL/subscriptions.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePicture = /* GraphQL */ `
+  subscription OnCreatePicture($owner: String!) {
+    onCreatePicture(owner: $owner) {
+      id
+      name
+      owner
+      visibility
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdatePicture = /* GraphQL */ `
+  subscription OnUpdatePicture($owner: String!) {
+    onUpdatePicture(owner: $owner) {
+      id
+      name
+      owner
+      visibility
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeletePicture = /* GraphQL */ `
+  subscription OnDeletePicture($owner: String!) {
+    onDeletePicture(owner: $owner) {
+      id
+      name
+      owner
+      visibility
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
